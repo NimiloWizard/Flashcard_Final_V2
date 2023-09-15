e850b5421e17e804d79eb121f8e96786ca871c88
NimiloWizard <135648843+NimiloWizard@users.noreply.github.com>
2023-09-15 08:11:09 +0800

    Update Flash_Card.js

--- Js/Flash_Card.js
+++ Js/Flash_Card.js
@@ -704,14 +704,18 @@ function saveDrawingState() {
 // Function to undo all drawing actions
 function undo() {
     if (currentStep >= 0) {
-      currentStep--;
-        const img = new Image();
-        img.src = history[currentStep];
-        img.onload = () => {
-            ctx.clearRect(0, 0, drawingArea.width, drawingArea.height);
-            ctx.drawImage(img, 0, 0);
-        };
+        // Clear the canvas
+        ctx.clearRect(0, 0, drawingArea.width, drawingArea.height);
         
+        // Redraw the history up to the current step
+        for (let i = 0; i <= currentStep - 1; i++) {
+            const img = new Image();
+            img.src = history[i];
+            ctx.drawImage(img, 0, 0);
+        }
+        
+        // Update the current step
+        currentStep--;
     }
 }
 
