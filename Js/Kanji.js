18349550a9118a6871bdc7d2b6a1e5c487bdda5a
NimiloWizard <135648843+NimiloWizard@users.noreply.github.com>
2023-09-17 16:32:45 +0800

    Update Kanji.js

--- Js/Kanji.js
+++ Js/Kanji.js
@@ -280,8 +280,7 @@ function open_screenGallery(bg_hide_Image) {
   w3_close()  //toggle aup the navbar after making a selection
 
   
-  const cardOnscreen = document.getElementById("card");
-  cardOnscreen.style.transform = "translateY(100%)";
+ 
 
 }
    function disableElement() {
